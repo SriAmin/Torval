693129edd75b5b8237e518ad8806561a9597afd2
Justin M <justinmacgregor4@gmail.com>
2021-11-15 23:58:35 -0500

    Chatbot screen updated - still wont connect

--- screens/ChatbotScreen.js
+++ screens/ChatbotScreen.js
@@ -1,175 +1,201 @@
-import React, { Component, useState } from 'react';
-import { StyleSheet, Text, TextInput, View, TouchableHighlight } from 'react-native';
-import axios from 'axios';
-import Voice from 'react-native-voice';
-import Tts from 'react-native-tts';
-const _backendEndpoint = 'https://computercompanion-chatbot.mybluemix.net';
+import React from 'react';
+import { StyleSheet, KeyboardAvoidingView, ScrollView, View, Text, TextInput, Button } from 'react-native';
+
+const serverUrl = 'https://computercompanion-chatbot.mybluemix.net'
+
+const Message = (props) => {
+  const style = props.fromInput ? styles.myText : styles.waText;
+
+  return (
+    <View style={styles.messageContainer}>
+      <Text style={style}>{props.text}</Text>
+    </View>
+  );
+};
 
 
 
 
+const ChatbotScreen = function ({ navigation }) {
+  const [input, setInput] = React.useState('');
+  const [session, setSession] = React.useState('');
+  const [messages, setMessages] = React.useState([]);
 
+  const getSession = () => {
+    return fetch(`${serverUrl}/api/session`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        } else {
+          return response.text();
+        }
+      })
+      .then(sessionId => {
+        setSession(sessionId);
+        return sessionId;
+      })
+  };
 
-class ChatbotScreen extends Component {
-    constructor(props) 
-    {
-        super(props);
+  const fetchMessage = (payload) => {
+    return fetch(`${serverUrl}/api/message`, {
+      method: 'POST',
+      mode: 'no-cors',
+      cache: 'no-cache',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload)
+    });
+  }
 
-        this.state = {
-            text: '',
-            status: '',
-            userPayload: '',
-            userSession: '',
-            userInput: ''
-        };
-
-        this.onStartButtonPress = this.onStartButtonPress.bind(this);
+  const handleMessageResponse = (response) => {
+    if (!response.ok) {
+      throw new Error(response.statusText || response.message || response.status);
+    } else {
+      return response.json().then(response => {
+        addMessages(response.generic);
+      })
     }
+  }
 
-    componentWillMount() {
-        this.getSession();
-    }
-
-
-
-
-
-
-    //GET WATSON SESSION
-    getSession = async () => {
-        const response = await axios.get(
-            `${_backendEndpoint}/api/session`,
-            this.state.userPayload,
-        );
-        this.init(response.data);
-        console.log(response)
+  const sendMessage = () => {
+    const payload = {
+      text: input.trim(),
+      sessionid: session
     };
 
+    addMessages([{ text: input }], true);
 
+    setInput('');
 
-
-
-
-    //ASSISTANT GREETING
-    init = async session => {
-        try 
-        {
-            const initialPayload = {
-                input: {
-                    message_type: 'text',
-                    text: '',
-                },
-            };
-
-            let response = await axios.post(`${_backendEndpoint}/api/message`, {
-                ...initialPayload,
-                ...session,
+    fetchMessage(payload)
+      .then(handleMessageResponse)
+      .catch(e => {
+        getSession()
+          .then((sessionId) => {
+            return fetchMessage({
+              text: payload.text,
+              sessionid: sessionId
             });
+          })
+          .then(handleMessageResponse)
+          .catch(err => {
+            console.log(err)
+            addMessages([{
+              text: 'Cannot connect to Chatbot'
+            }]);
+          });
+      });
+  };
 
-            this.setState({ userSession: session });
-            this.setState({ text: response.data.output.generic[0].text });
-            this.setState({ userPayload: response.data });
-        } 
+  const addMessages = (messages, fromInput) => {
+    const result = messages.map((r, i) => {
+      return {
+        text: r.text,
+        fromInput: fromInput
+      };
+    });
 
-        catch (err) 
-        {
-            console.log('Failed to retrive data from Watson API', err);
-        }
-    };
+    setMessages(msgs => [
+      ...msgs,
+      ...result
+    ]);
+  };
 
+  React.useEffect(() => {
+    navigation.addListener('focus', () => {
+      getSession();
+    })
+  }, []);
 
+  return (
+    <View style={styles.outerContainer}>
+      <KeyboardAvoidingView
+        style={styles.innerContainer}
+        behavior='height'>
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+          {messages.map((message, i) => {
+            message.key = `${(new Date()).getTime()}-${i}`;
+            return <Message {...message} />
+          })}
+        </ScrollView>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.textInput}
+            value={input}
+            onChangeText={setInput}
+            onSubmitEditing={sendMessage}
+            returnKeyType='send'
+            enablesReturnKeyAutomatically={true}
+            placeholder='Ask a question...'
+            blurOnSubmit={false}
+          />
+          <View style={styles.submitButton}>
+            {input !== '' && <Button title='Send' onPress={sendMessage} />}
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    </View>
+  );
+};
 
-
-
-
-    //GRAB USER INPUT TO SEND TO WATSON
-     onStartButtonPress = userInput => {
-         this.sendMessage(this.state.userInput);
-         console.log(this.state.userInput);
-     };
-
-
-
-
-
-
-
-
-    //SEND USER INPUT TO WATSON
-    sendMessage = async payload => {
-        try 
-        {
-            let { userSession } = this.state;
-            let inputPayload = {
-                input: {
-                    message_type: 'text',
-                    text: payload,
-                },
-            };
-
-            let responseData = { ...inputPayload, ...userSession };
-            let response = await axios.post(
-                `${_backendEndpoint}/api/message`,
-                responseData,
-            );
-            this.setState({ text: response.data.output.generic[0].text });
-            Tts.speak(response.data.output.generic[0].text);
-        } 
-        
-        catch (err) 
-        {
-            console.log('Failed to send data to Watson API', err);
-        }
-    };
-
-
-
-
-
-
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>Welcome to Computer Companion</Text>
-                <TextInput style={{height: 40}} placeholder="Type here to talk to bot!" 
-                onChangeText={(userInput) => this.setState({userInput})}/>
-
-                <TouchableHighlight
-                    style={{
-                        borderColor: 'black',
-                        borderWidth: 1,
-                        width: 100,
-                        height: 50,
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}
-                    underlayColor={'gray'}
-                    onPressIn={e => this.onStartButtonPress(e)}>
-                    <Text>Talk</Text>
-                </TouchableHighlight>
-
-                <Text style={{ fontSize: 20, color: 'red' }}>{this.state.text}</Text>
-                <Text style={{ fontSize: 20, color: 'blue' }}>{this.state.status}</Text>
-            </View>
-        );
-    }
-}
 export default ChatbotScreen;
 
 
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-        padding: 20,
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
+  outerContainer: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#FFF'
+  },
+  innerContainer: {
+    width: '100%',
+    height: '100%'
+  },
+  scrollContainer: {
+    flex: 1,
+    width: '100%',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    paddingHorizontal: 16,
+    paddingBottom: 5
+  },
+  messageContainer: {
+    flexDirection: 'column',
+    marginTop: 10,
+    alignItems: 'stretch',
+    justifyContent: 'flex-start'
+  },
+  waText: {
+    backgroundColor: '#D0E2FF',
+    padding: 10,
+    alignSelf: 'flex-start',
+    maxWidth: '85%'
+  },
+  myText: {
+    backgroundColor: '#F1F0EE',
+    padding: 10,
+    alignSelf: 'flex-end',
+    maxWidth: '80%'
+  },
+  inputContainer: {
+    backgroundColor: '#F1F0EE',
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 14
+  },
+  textInput: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 16,
+    elevation: 2,
+    paddingRight: 70,
+    marginBottom: 25
+  },
+  submitButton: {
+    position: 'absolute',
+    right: 24,
+    bottom: 47
+  }
 });
--- screens/ChatbotScreenBAK.js
+++ screens/ChatbotScreenBAK.js
@@ -1,101 +1,179 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, TextInput, KeyboardAvoidingView, Image, Button, Dimensions, TouchableOpacity } from 'react-native';
-import { Ionicons, AntDesign } from '@expo/vector-icons';
-import { Conversation } from 'react-native-watson'
+import React, { Component, useState } from 'react';
+import { StyleSheet, Text, TextInput, View, TouchableHighlight } from 'react-native';
+import axios from 'axios';
+import Voice from 'react-native-voice';
+import Tts from 'react-native-tts';
+var AssistantV2 = require('ibm-watson/assistant/v2'); // watson sdk
+const { IamAuthenticator, BearerTokenAuthenticator } = require('ibm-watson/auth');
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+const _backendEndpoint = 'https://computercompanion-chatbot.mybluemix.net';
 
-const ChatbotScreen = () => {
-    const [text, onChangeText] = useState("");
-    const [sent, onChangeSent] = useState(false);
 
-    Message = () => {
-        if (!sent)
-            return <View></View>;
-        else
-            return (
-                <View style={{
-                    backgroundColor: "#0078fe",
-                    padding:10,
-                    marginLeft: '45%',
-                    borderRadius: 5,
-                    marginBottom: 15,
-                    marginTop: 5,
-                    marginRight: "5%",
-                    maxWidth: '50%',
-                    alignSelf: 'flex-end',
-                    maxWidth: 500,
-                    borderRadius: 20,
-                  }}> 
-                <Text style={{ fontSize: 16, color: "#fff", }}>{text}</Text>
-                <View style={styles.rightArrow}></View>
-                <View style={styles.rightArrowOverlap}></View>
-                </View>
-            );
+
+
+
+
+
+class ChatbotScreen extends Component {
+    constructor(props) 
+    {
+        super(props);
+
+        this.state = {
+            text: '',
+            status: '',
+            userPayload: '',
+            userSession: '',
+            userInput: ''
+        };
+
+        this.onStartButtonPress = this.onStartButtonPress.bind(this);
     }
 
-    return (
-        <KeyboardAvoidingView style={styles.container} behavior="height" enabled>
-            <View style={[{flexDirection: "row"}]}>
-                <TextInput
-                        style={styles.input}
-                        onChangeText={onChangeText}
-                        value = {text}
-                        placeholder = "Enter your message here"
-                />
-                <TouchableOpacity onPress={() => {onChangeSent(true)}}>
-                    <AntDesign name="arrowup" size={32} color="black" style={[{marginTop: 12.5}]}/>            
-                </TouchableOpacity>
+    componentWillMount() {
+        this.getSession();
+    }
+
+
+
+
+
+
+    //GET WATSON SESSION
+    getSession = async () => {
+        const response = await axios.get(
+            `${_backendEndpoint}/api/session`,
+            this.state.userPayload,
+        );
+        this.init(response.data);
+        console.log(response)
+    };
+
+
+
+
+
+
+    //ASSISTANT GREETING
+    init = async session => {
+        try 
+        {
+            const initialPayload = {
+                input: {
+                    message_type: 'text',
+                    text: '',
+                },
+            };
+
+            let response = await axios.post(`${_backendEndpoint}/api/message`, {
+                ...initialPayload,
+                ...session,
+            });
+
+            this.setState({ userSession: session });
+            this.setState({ text: response.data.output.generic[0].text });
+            this.setState({ userPayload: response.data });
+        } 
+
+        catch (err) 
+        {
+            console.log('Failed to retrive data from Watson API', err);
+        }
+    };
+
+
+
+
+
+
+    //GRAB USER INPUT TO SEND TO WATSON
+     onStartButtonPress = userInput => {
+         this.sendMessage(this.state.userInput);
+         console.log(this.state.userInput);
+     };
+
+
+
+
+
+
+
+
+    //SEND USER INPUT TO WATSON
+    sendMessage = async payload => {
+        try 
+        {
+            let { userSession } = this.state;
+            let inputPayload = {
+                input: {
+                    message_type: 'text',
+                    text: payload,
+                },
+            };
+
+            let responseData = { ...inputPayload, ...userSession };
+            let response = await axios.post(
+                `${_backendEndpoint}/api/message`,
+                responseData,
+            );
+            this.setState({ text: response.data.output.generic[0].text });
+            Tts.speak(response.data.output.generic[0].text);
+        } 
+        
+        catch (err) 
+        {
+            console.log('Failed to send data to Watson API', err);
+        }
+    };
+
+
+
+
+
+
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>Welcome to Computer Companion</Text>
+                <TextInput style={{height: 40}} placeholder="Type here to talk to bot!" 
+                onChangeText={(userInput) => this.setState({userInput})}/>
+
+                <TouchableHighlight
+                    style={{
+                        borderColor: 'black',
+                        borderWidth: 1,
+                        width: 100,
+                        height: 50,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                    underlayColor={'gray'}
+                    onPressIn={e => this.onStartButtonPress(e)}>
+                    <Text>Talk</Text>
+                </TouchableHighlight>
+
+                <Text style={{ fontSize: 20, color: 'red' }}>{this.state.text}</Text>
+                <Text style={{ fontSize: 20, color: 'blue' }}>{this.state.status}</Text>
             </View>
-            <Message />
-        </KeyboardAvoidingView>
-    )
+        );
+    }
 }
+export default ChatbotScreen;
+
+
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: "column-reverse",
-        backgroundColor: '#fff',
-      },
-      messageContainer: {
-          marginTop: 10,
-          flexDirection: "row",
-      },
-      img: {
-        width: 15,
-        height: 15,
-      },
-      input: {
-        height: 40,
-        width: windowWidth - 80,
-        margin: 12,
-        borderWidth: 1,
-        padding: 10,
-      },
-      rightArrow: {
-        position: "absolute",
-        backgroundColor: "#0078fe",
-        //backgroundColor:"red",
-        width: 20,
-        height: 25,
-        bottom: 0,
-        borderBottomLeftRadius: 25,
-        right: -10
-      },
-      
-      rightArrowOverlap: {
-        position: "absolute",
-        backgroundColor: "#fff",
-        //backgroundColor:"green",
-        width: 20,
-        height: 35,
-        bottom: -6,
-        borderBottomLeftRadius: 18,
-        right: -20
-      
-      },
-})
-
-export default ChatbotScreen;
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+        padding: 20,
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+});
