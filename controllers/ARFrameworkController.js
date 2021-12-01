f56b6178c7144b8cc5753b925c78b3af0960ac07
Sri Amin <sri.amin@gmail.com>
2021-12-01 18:28:16 -0500

    Changed the AR Tutorial to Show a Laptop Model

--- controllers/ARFrameworkController.js
+++ controllers/ARFrameworkController.js
@@ -2,31 +2,54 @@ import React, {useState} from 'react';
 import {StyleSheet} from 'react-native';
 import {
   ViroARScene,
-  ViroText,
+  Viro3DObject,
+  ViroBox,
+  ViroAmbientLight,
   ViroConstants,
+  ViroARPlane,
+  ViroARTrackingTargets,
+  ViroARImageMarker,
   ViroARSceneNavigator,
 } from '@viro-community/react-viro';
 
-const HelloWorldSceneAR = () => {
+const TutorialSceneAR = () => {
   const [text, setText] = useState('Initializing AR...');
 
   function onInitialized(state, reason) {
-    console.log('guncelleme', state, reason);
     if (state === ViroConstants.TRACKING_NORMAL) {
-      setText('Hello World!');
+      console.log("AR is Ready")
+      setText('Hello, its good to meet you!');
     } else if (state === ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
     }
   }
 
+  // Outside of the render function, register the target
+
+  ViroARTrackingTargets.createTargets({
+    "targetOne" : {
+      source : require('../models/teslaLogo.png'),
+      orientation : "Up",
+      physicalWidth : 0.1 // real world width in meters
+    },
+  });
+
   return (
     <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroText
-        text={text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
-      />
+      <ViroARPlane minHeight={.5} minWidth={.5} alignment={"Horizontal"}>
+        <Viro3DObject 
+            source={require('../models/notebook.obj')}
+            resources={[require('../models/Lowpoly_Notebook_2.mtl'),
+              require('../models/textures/Lowpoly_Laptop_1.jpg'),
+              require('../models/textures/Lowpoly_Laptop_2.jpg'),
+              require('../models/textures/Lowpoly_Laptop_Nor_1.jpg'),
+              require('../models/textures/Lowpoly_Laptop_Nor_2.jpg'),]}
+            position={[0.0, 0.0, 0.0]}
+            scale={[0.1, 0.1, 0.1]}
+            type="OBJ"
+          />
+        <ViroAmbientLight color="#FFFFFF" />      
+      </ViroARPlane>
     </ViroARScene>
   );
 };
@@ -36,7 +59,7 @@ const ARFrameworkController = () => {
     <ViroARSceneNavigator
       autofocus={true}
       initialScene={{
-        scene: HelloWorldSceneAR,
+        scene: TutorialSceneAR,
       }}
       style={styles.f1}
     />
@@ -44,7 +67,7 @@ const ARFrameworkController = () => {
 };
 
 var styles = StyleSheet.create({
-  f1: {flex: 1},
+  f1: {flex: 2},
   helloWorldTextStyle: {
     fontFamily: 'Arial',
     fontSize: 30,
--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,10 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.assetExts.push('obj');
+defaultConfig.resolver.assetExts.push('mtl');
+defaultConfig.resolver.assetExts.push('vrx');
+
+module.exports = defaultConfig;
