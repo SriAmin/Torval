16b324f461ecceed144bcabc43a1f73465990904
Sri Amin <sri.amin@gmail.com>
2021-12-15 18:39:54 -0500

    Reverted changes and fixed the application

--- screens/Chatbot/ChatbotScreen.js
+++ screens/Chatbot/ChatbotScreen.js
@@ -1,29 +1,116 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState} from 'react';
 import { WebView } from 'react-native-webview';
 import { StyleSheet, Text, TextInput, View, TouchableHighlight, Platform } from 'react-native';
 
-class ChatbotScreen extends Component {
-  function = function () {
-    console.log("Hello")
+const styles = StyleSheet.create({
+  socialForumButton: {
+      backgroundColor: 'purple',
+      height: 50,
+      width: 50,
+      borderRadius: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+      position: 'absolute',
+      left:20,
+      bottom:10,
+      shadowColor: "#000000",
+      shadowOpacity: 0.8,
+      shadowRadius: 2,
+      shadowOffset: {
+          height: 1,
+          width: 0
+      }
+  },
+
+  arButton: {
+    backgroundColor: 'purple',
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    left:80,
+    bottom:10,
+    shadowColor: "#000000",
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    shadowOffset: {
+        height: 1,
+        width: 0
+    }
+}
+});
+
+
+
+const ChatbotScreen = ({navigation}) => {
+
+  const [forumCheck, setForumCheck] = useState(false)
+
+  const navigateToSocialForum = () => {
+    navigation.navigate('Forum')
   }
-  render() {
 
-    // return (
-    //     <WebView source={{uri: 'https://computercompanion-chatbot.mybluemix.net'}}/>
-    // );
+  const navigateToAR = () => {
+    navigation.navigate('Tutorials')
+  }
+  
+  const onMessage = (data) => {
+    var htmlString = data;
+    var forumWordLocation = htmlString.search("forum");
 
-    //return (Platform.OS === 'web' ? <iframe src='https://computercompanion-chatbot.mybluemix.net' frameborder="0" height="100%" width="100%"/> : <WebView source={{uri: 'https://computercompanion-chatbot.mybluemix.net'}}/>)
+    if (forumWordLocation == -1) {
+      //hide button
+      setForumCheck(false);
+    } else {
+      //show button
+      setForumCheck(true);
+    }
+  }
 
+  const ForumButton = () => {
+
+    if (forumCheck == false) {
+      return <View></View>;
+    } else {
+      return (<View>
+        <TouchableHighlight style={styles.socialForumButton} underlayColor='#ff7043' onPress={()=>{navigateToSocialForum()}}>
+          <Text style={{fontSize: 10, color: 'white'}}>FORUM</Text>
+        </TouchableHighlight>
+
+        <TouchableHighlight style={styles.arButton} underlayColor='#ff7043' onPress={()=>{navigateToAR()}}>
+          <Text style={{fontSize: 10, color: 'white'}}>AR</Text>
+        </TouchableHighlight>
+      </View>)
+    }
+  }
+
+    //Code to inject: Gets inner html of webpage and updates every 5 seconds
+    const jsCode = 'function refresh(){window.ReactNativeWebView.postMessage(document.documentElement.innerHTML);setTimeout(refresh, 2000);}refresh();'
+    
     return Platform.OS === "web" ? (
       <iframe frameborder="0" src="https://computercompanion-chatbot.mybluemix.net" height={'100%'} width={'100%'} />
     ) : (
+
       <View style={{ flex: 1 }}>
+
+        {/* WebView for chatbot */}
         <WebView
-          source={{ uri: "https://computercompanion-chatbot.mybluemix.net" }}
-          style={{marginTop: 22, flex: 1}}
-        />
+            javaScriptEnabled={true}
+            javaScriptEnabledAndroid={true}
+            injectedJavaScript={jsCode}
+            source={{ uri: "https://computercompanion-chatbot.mybluemix.net" }}
+            style={{marginTop: 22, flex: 1}}
+            onMessage={event => onMessage(event.nativeEvent.data)}
+          />
+
+        {/* forum button */}
+        <ForumButton />
+
       </View>
     );
   }
-}
+
+
 export default ChatbotScreen;
