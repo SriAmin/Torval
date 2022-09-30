f9e0800225339527bf3dd075b49b3a7f7fff8f22
Sri Amin <sri.amin@gmail.com>
2022-09-30 18:24:48 -0400

    Made a global stylesheet in the application

---
+++ components/Styles.js
@@ -0,0 +1,11 @@
+'use strict';
+import { StyleSheet } from 'react-native';
+
+module.exports = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+     },
+});
