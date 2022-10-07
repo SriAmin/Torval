e259be2385ecc0e9969ab81e07efe51f1041f7c2
Sri Amin <sri.amin@gmail.com>
2022-10-07 17:37:07 -0400

    Implemented the ability to jump in between steps

--- screens/ARTutorials/TutorialView.js
+++ screens/ARTutorials/TutorialView.js
@@ -19,21 +19,81 @@ import TutorialSceneAR2 from '../../components/TutorialSceneAR2';
 
 //JSON file contains the tutorial information such as teh instructions and models
 const tutorialInstructions = [
-    "Welcome, ready to build your PC, the first step is to open the CPU latch on the motherboard, and carefully place the CPU in the bracket, make sure the triangles on the corner matchs, once the CPU fits in snug, put the metal latch down to lock the CPU in place",
-    "On the RAM area of the motherboard, pull the handles out, place the RAM in the slot, push down until the handles click back into place. Make sure the sticks are aligned correctly.",
-    "To attach the CPU Fan, if there isn't thermal paste on the fan, place a drop of it on the CPU, place the fan on top of the CPU and screw it in with the screws given from the CPU Fan.",
-    "Grab the PC case, and unscrew the sides, we want to open the sides of the PC case to place the components inside. Unscrew and remove both side panels.",
-    "On the Power Supply, input the required power cables on the back, these are Motherboard, CPU, PCI/E, and SATA, make sure you put the right cables, refer to the manual if you need to.",
-    "Place the Power Supply in the PSU slot on the PC, the power switch should be exposed from the back of the PC, using the screws provided from the power supply, screw the power supply to the case",
-    "Remove any unwanted standoffs and place the motherboard overtop the existing ones, make sure the number of standoffs are the same as the number of holes on the motherboard that you can screw in, place the motherboard in, screw it, and attach it to the PC Case.",
-    "If you have a hardrive (HDD), Turn the PC to face the other side, and slide the HDD into the slot indicated above, it should easily slide and lock into place",
-    "If you have a solid state drive (SDD), place the SDD into the bracket shown above, screw it into place using the screws provided by the SDD bracket.",
-    "Turn the PC back around to face the side of the motherboard, unscrew and remove the side grill prices where the graphics card will be, don't remove all of them, just the ones needed for the graphics card to poke through",
-    "On the PCI/E handle, where the graphics card (GPU) will be placed, pull back the handle and place the GPU over it, push it into place until the handle snaps back and locks in the GPU, make sure the HDMI inputs poke through the end of the PC",
-    "To add power to the motherboard, CPU and GPU, get the ends of the motherboard cable, CPU cable and PCI/E cable (the ones attached to the PSU) and plug them into the slots shown above, refer to the motherboard manual as well if you have trouble finding the spot to plug the power cables in",
-    "Turn the PC around and plug the SATA power (from PSU) and SATA data cable (included with motherboard) into the HDD and SDD, turn the PC around and plug the other ends of the SATA data cable into the motherboard",
-    "Plug in the power cables that came with the PC Case, this should allow you to use the input on your PC (Power button and USB), we highly suggest you to refer to the motherboard manual in regards to how you should plug it, as they change per model",
-    "Finally, screw back in the side panels of the PC, flip the switch on the PSU, plug the PC power cable (comes with the PC), and press on the power button. Congratualtions, you've built PC with custom components, for any issues that occur please use the forum threads to help from our community."
+    {
+        "key" : 1,
+        "title" : "Step 1: CPU",
+        "step" : "Welcome, ready to build your PC, the first step is to open the CPU latch on the motherboard, and carefully place the CPU in the bracket, make sure the triangles on the corner matchs, once the CPU fits in snug, put the metal latch down to lock the CPU in place",
+    },
+    {
+        "key" : 2,
+        "title" : "Step 2: RAM",
+        "step" : "On the RAM area of the motherboard, pull the handles out, place the RAM in the slot, push down until the handles click back into place. Make sure the sticks are aligned correctly.",
+    },
+    {
+        "key" : 3,
+        "title" : "Step 3: CPU Fan",
+        "step" : "To attach the CPU Fan, if there isn't thermal paste on the fan, place a drop of it on the CPU, place the fan on top of the CPU and screw it in with the screws given from the CPU Fan.",
+    },
+    {
+        "key" : 4,
+        "title" : "Step 4: Open Case",
+        "step" : "Grab the PC case, and unscrew the sides, we want to open the sides of the PC case to place the components inside. Unscrew and remove both side panels.",
+    },
+    {
+        "key" : 5,
+        "title" : "Step 5: PSU Cables",
+        "step" : "On the Power Supply, input the required power cables on the back, these are Motherboard, CPU, PCI/E, and SATA, make sure you put the right cables, refer to the manual if you need to.",
+    },
+    {
+        "key" : 6,
+        "title" : "Step 6: PSU",
+        "step" : "Place the Power Supply in the PSU slot on the PC, the power switch should be exposed from the back of the PC, using the screws provided from the power supply, screw the power supply to the case",
+    },
+    {
+        "key" : 7,
+        "title" : "Step 7: Motherboard",
+        "step" : "Remove any unwanted standoffs and place the motherboard overtop the existing ones, make sure the number of standoffs are the same as the number of holes on the motherboard that you can screw in, place the motherboard in, screw it, and attach it to the PC Case.",
+    },
+    {
+        "key" : 8,
+        "title" : "Step 8: HDD",
+        "step" : "If you have a hardrive (HDD), Turn the PC to face the other side, and slide the HDD into the slot indicated above, it should easily slide and lock into place",
+    },
+    {
+        "key" : 9,
+        "title" : "Step 9: SDD",
+        "step" : "If you have a solid state drive (SDD), place the SDD into the bracket shown above, screw it into place using the screws provided by the SDD bracket.",
+    },
+    {
+        "key" : 10,
+        "title" : "Step 10: GPU Covers",
+        "step" : "Turn the PC back around to face the side of the motherboard, unscrew and remove the side grill prices where the graphics card will be, don't remove all of them, just the ones needed for the graphics card to poke through",
+    },
+    {
+        "key" : 11,
+        "title" : "Step 11: GPU",
+        "step" : "On the PCI/E handle, where the graphics card (GPU) will be placed, pull back the handle and place the GPU over it, push it into place until the handle snaps back and locks in the GPU, make sure the HDMI inputs poke through the end of the PC",
+    },
+    {
+        "key" : 12,
+        "title" : "Step 12: Power Components",
+        "step" : "To add power to the motherboard, CPU and GPU, get the ends of the motherboard cable, CPU cable and PCI/E cable (the ones attached to the PSU) and plug them into the slots shown above, refer to the motherboard manual as well if you have trouble finding the spot to plug the power cables in",
+    },
+    {
+        "key" : 13,
+        "title" : "Step 13: Connect HDD and SDD",
+        "step" : "Turn the PC around and plug the SATA power (from PSU) and SATA data cable (included with motherboard) into the HDD and SDD, turn the PC around and plug the other ends of the SATA data cable into the motherboard",
+    },
+    {
+        "key" : 14,
+        "title" : "Step 14: PC Power Cables",
+        "step" : "Plug in the power cables that came with the PC Case, this should allow you to use the input on your PC (Power button and USB), we highly suggest you to refer to the motherboard manual in regards to how you should plug it, as they change per model",
+    },
+    {
+        "key" : 15,
+        "title" : "Step 15: Close PC Case",
+        "step" : "Finally, screw back in the side panels of the PC, flip the switch on the PSU, plug the PC power cable (comes with the PC), and press on the power button. Congratualtions, you've built PC with custom components, for any issues that occur please use the forum threads to help from our community."
+    },
 ]
 
 const TutorialView = ({ navigation, route }) => {
@@ -48,13 +108,11 @@ const TutorialView = ({ navigation, route }) => {
     Return:
     View component with ViroARSceneNavigator and 2D UI components
     */
-   
-    const [instruction, setInstruction] = useState(tutorialInstructions[0])
     const [tutorialStep, setTutorialStep] = useState(1);
     const [stepMenu, setStepMenu] = useState(false);
     const arSceneNav = useRef(null);
     const slideInAnim = useRef(new Animated.Value(-170.0)).current;
-
+    
     const slideIn = () => {
         console.log("Step Menu Active: " + stepMenu);
         if (stepMenu) {
@@ -65,7 +123,7 @@ const TutorialView = ({ navigation, route }) => {
             }).start();
         } else {
             Animated.timing(slideInAnim, {
-                toValue: 1,
+                toValue: 50,
                 duration: 250,
                 useNativeDriver: false,
             }).start();
@@ -75,7 +133,6 @@ const TutorialView = ({ navigation, route }) => {
 
     const nextStep = async () => {
         setTutorialStep(tutorialStep+1);
-        setInstruction(tutorialInstructions[tutorialStep]);
         const tempStep = tutorialStep+1;
         arSceneNav.current.arSceneNavigator.pop()
         arSceneNav.current.arSceneNavigator.push({ 
@@ -87,7 +144,6 @@ const TutorialView = ({ navigation, route }) => {
 
     const previousStep = async () => {
         setTutorialStep(tutorialStep-1);
-        setInstruction(tutorialInstructions[tutorialStep-2]);
         const tempStep = tutorialStep-1;
         arSceneNav.current.arSceneNavigator.pop()
         arSceneNav.current.arSceneNavigator.push({ 
@@ -95,6 +151,17 @@ const TutorialView = ({ navigation, route }) => {
             passProps: { modelName: "Step" + tempStep.toString() } 
         })
         console.log("Popped out Scene");
+    }
+
+    const jumpStep = async (stepIndex) => {
+        setTutorialStep(stepIndex);
+        arSceneNav.current.arSceneNavigator.pop()
+        arSceneNav.current.arSceneNavigator.push({ 
+            scene: TutorialSceneAR, 
+            passProps: { modelName: "Step" + stepIndex.toString() } 
+        })
+        console.log("Jumped to Scene" + tutorialStep);
+        slideIn();
     }
 
     return (
@@ -112,21 +179,9 @@ const TutorialView = ({ navigation, route }) => {
             <TouchableOpacity style={styles.backButton} onPress={navigation.goBack}>
                 <Ionicons name="arrow-back-circle-outline" size={40} color="white" />
             </TouchableOpacity>
-            <Animated.View style={[styles.stepButton, {right: slideInAnim}]}>
-                <TouchableOpacity onPress={() => {slideIn()}}>
-                    <Feather name="grid" size={40} color="white" />
-                </TouchableOpacity>
-                <View style={styles.stepMenu}>
-                    <FlatList 
-                        data={tutorialInstructions}
-                        renderItem={({index}) => {
-                            return <Text style={styles.stepText}>Step {index + 1}</Text>
-                        }}
-                    />
-                </View>
-            </Animated.View>
             <View style={styles.uiView}>
-                <Text style={styles.instruction}>{instruction}</Text>
+                <Text style={styles.instruction}>{tutorialInstructions[tutorialStep-1].title}</Text>
+                <Text style={styles.instruction}>{tutorialInstructions[tutorialStep-1].step}</Text>
                 <View style={styles.buttonGroup}>
                     <TouchableOpacity style={styles.button} onPress={() => { 
                         previousStep()
@@ -140,6 +195,24 @@ const TutorialView = ({ navigation, route }) => {
                     </TouchableOpacity>
                 </View>
             </View>
+            <Animated.View style={[styles.stepButton, {right: slideInAnim}]}>
+                <TouchableOpacity onPress={() => {slideIn()}}>
+                    <Feather name="grid" size={40} color="white" />
+                </TouchableOpacity>
+                <View style={styles.stepMenu}>
+                    <FlatList 
+                        data={tutorialInstructions}
+                        renderItem={({item, index}) => {
+                            return (
+                                <TouchableOpacity onPress={() => {jumpStep(index+1)}}>
+                                    <Text style={styles.stepText}>{item.title}</Text>
+                                </TouchableOpacity>
+                            )
+                        }}
+                        keyExtractor={item => item.key}
+                    />
+                </View>
+            </Animated.View>
         </View>
     );
 }
@@ -166,12 +239,12 @@ const styles = StyleSheet.create({
     stepMenu: {
         marginLeft: 25,
         backgroundColor: "white",
-        width: '75%',
-        marginRight: -150,
+        width: '125%',
+        marginRight: -350,
     },
     stepText: {
         color: "#7b42f5",
-        fontSize: 18,
+        fontSize: 16,
         padding: 8
     },
     uiView: {
