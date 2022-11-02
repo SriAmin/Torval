1d8d5e7b8749a8f41d43539f54f2e96f9a64c69e
JustinMacGregor <justinmacgregor4@gmail.com>
2022-11-02 14:45:27 -0400

    Added delete button when user ismod

--- screens/SocialForum/ThreadDetailScreen.js
+++ screens/SocialForum/ThreadDetailScreen.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, FlatList, Button, ActivityIndicator} from 'react-native';
-import {db} from "../../config/firebase";
+import {auth, db} from "../../config/firebase";
+import {doc, getDoc} from "firebase/firestore";
 
 //This is used to conditional render the list if there are comments for the thread or if none exist at the moment
 const CommentList = ({data}) => {
@@ -45,9 +46,27 @@ const TutorialButton = ({followedTutorial, navigation}) => {
 const ThreadDetailScreen = ({ navigation, route, isFocused}) => {
     const [thread, setThread] = useState(null)
     const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState({});
+    const deleteBtn = <Button style={{marginTop: 20}} title="DeleteBtn"><Text>DeleteBtn</Text></Button>;
+
+    const getUser = async () => {
+        const docRef = doc(db, "Users", auth.currentUser.email);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            const userData = docSnap.data();
+            setUser(docSnap.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }
 
     useEffect(() => {
         (async () => {
+            getUser();
+            const isMod = user.isMod;
+
             const firestoreThread = await db.collection('Threads').doc(route.params.threadId).get()
             setThread(firestoreThread.data());
             setLoading(false);
@@ -61,6 +80,13 @@ const ThreadDetailScreen = ({ navigation, route, isFocused}) => {
             <View style={styles.container}>
                 <View style={[{borderBottomWidth: 1, marginBottom: 10, padding: 15}]}>
                     <Text style={[{fontSize: 18}]}>{thread.title}</Text>
+
+                    {
+                        user.isMod
+                        ? <Button style={{marginTop: 20}} title="DeleteBtn"><Text>DeleteBtn</Text></Button>
+                        : <View></View>
+                    }
+
                     <Text style={styles.threadAuthor}>{thread.author}</Text>
                     <Text style={styles.threadTitle}>{thread.description}</Text>
                     <TutorialButton followedTutorial={thread.followedTutorial} navigation={navigation}/>
