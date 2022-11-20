231656ad23e0920c2c8f4f3d491df4a75de3328e
JustinMacGregor <justinmacgregor4@gmail.com>
2022-11-20 14:14:47 -0500

    VOTING SYSTEM WORKS

--- components/VoteComponent.js
+++ components/VoteComponent.js
@@ -7,11 +7,8 @@ import { db } from "../config/firebase";
 export default function VoteComponent({ comment, threadId, commentArray }) {
   const [upvoteChecked, setUpvoteChecked] = React.useState(false);
   const [downvoteChecked, setDownvoteChecked] = React.useState(false);
-  const [array, setArray] = React.useState(commentArray);
+  const [array] = React.useState(commentArray);
   const [karma, setKarma] = React.useState(0);
-
-  let promisedKarmaSetState = karma =>
-    new Promise(resolve => this.setState(karma, resolve));
 
   //initialize variable on startup
   React.useEffect(async () => {
@@ -19,11 +16,11 @@ export default function VoteComponent({ comment, threadId, commentArray }) {
     setKarma(comment.karma);
 
     //if user is in the array of upvoters, set the upvoteChecked to true, vice versa for downvoters
-    if (comment.upvoters.includes(db.auth().currentUser.email)) {
-      setUpvoteChecked(true);
-    } else if (comment.downvoters.includes(db.auth().currentUser.email)) {
-      setDownvoteChecked(true);
-    }
+    // if (comment.upvoters.includes(db.auth().currentUser.email)) {
+    //   setUpvoteChecked(true);
+    // } else if (comment.downvoters.includes(db.auth().currentUser.email)) {
+    //   setDownvoteChecked(true);
+    // }
   }, []);
 
   //updating the vote in firestore
@@ -32,7 +29,7 @@ export default function VoteComponent({ comment, threadId, commentArray }) {
     //for every comment in the array
     for (let i = 0; i < array.length; i++) {
       //if the comment id matches the comment id of the comment that was passed in, update the karma
-      if (array[i].id == comment.id) {
+      if (array[i].id === comment.id) {
         array[i].karma++;
         setKarma(array[i].karma);
         //replace whole comments array in document with the updated array
@@ -52,7 +49,7 @@ export default function VoteComponent({ comment, threadId, commentArray }) {
     //for every comment in the array
     for (let i = 0; i < array.length; i++) {
       //if the comment id matches the comment id of the comment that was passed in, update the karma
-      if (array[i].id == comment.id) {
+      if (array[i].id === comment.id) {
         array[i].karma--;
         setKarma(array[i].karma);
 
@@ -69,10 +66,16 @@ export default function VoteComponent({ comment, threadId, commentArray }) {
 
   const onUpvotePressed = async () => {
     if (downvoteChecked) {
+      await updateUpvoteFirestore();
       setDownvoteChecked(false);
     }
 
+    if (upvoteChecked) {
+      await updateDownvoteFirestore();
+    }
+
     await setUpvoteChecked(!upvoteChecked);
+
     if (!upvoteChecked) {
       await updateUpvoteFirestore();
     }
@@ -80,8 +83,14 @@ export default function VoteComponent({ comment, threadId, commentArray }) {
 
   const onDownvotePressed = async () => {
     if (upvoteChecked) {
+      await updateDownvoteFirestore();
       setUpvoteChecked(false);
     }
+
+    if (downvoteChecked) {
+      await updateUpvoteFirestore();
+    }
+
     await setDownvoteChecked(!downvoteChecked);
 
     if (!downvoteChecked) {
